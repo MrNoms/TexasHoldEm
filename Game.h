1abf81f78aed3418ac817fde35523b7b8bd73f86
Skyler <skylernovak@yahoo.com>
2019-04-22 13:32:20 -0700

    Changes to Game.h
    more comments, splitPot function, CalcHand function, and handRank enumeration

--- Game.h
+++ Game.h
@@ -4,21 +4,34 @@
 #include "Stack.h"		//of this game, rather than generic code
 
 class Player {};
+	// Private: handRank, hand
 class Deck {};	//would use stack
 
 class Game {
-	Deck PlayCards;
-	Card CommCards[5];
+public:
+	Deck PlayCards; // Deck of game cards? 
+		// functions to initialize deck, shuffle, pass out
+	int PayIn(Player *);	// input circular list of players. return to Pot
+	int PayOut(Player *);	// mutator for Pot
+	int splitPot(Player *, Player *); // input two players who split the pot
+
+	void calcHand(Player *); // input player circular list, traverse list & assign hand value to each player
+
+private:
+	enum RoundPhase { PRE_FLOP, FLOP, TURN, RIVER, SHOW };
+	enum handRank { HIGH_CARD, PAIR, TWO_PAIR, THREE_OF_KIND, STRAIGHT, FLUSH, FULL_HOUSE, FOUR_OF_KIND, STRAIGHT_FLUSH, ROYAL_FLUSH };
+	int const Rounds = 10;
+
+	Card CommCards[5]; // Array for the "River"
+		// need functions to add cards from deck to this & to empty array
+
 	CircleList<Player> Table;	//the Player class is coming
+	Player * Add2Table();		//return type and arguments up for debate
+	void Kick(Player *);
 	Player * DealerButton;		//Pointer type may have to be tweaked
 	Player * BigBlind;			//May not
 	Player * SmallBlind;		//keep these
-	enum RoundPhase {PRE_FLOP, FLOP, TURN, RIVER, SHOW};
-	int const Rounds = 10;
+
 	int Pot;
-	int Ante;
-	int PayOut(Player *);
-	int PayIn();
-	Player * Add2Table();			//return type and arguments up for debate
-	void Kick(Player *);
+	int Ante;	// Do we need this if we're using blinds?
 };
