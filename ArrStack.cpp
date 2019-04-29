c9aaeaa84b1d4d029bedef2553a63b5bf91e137e
Rayshaun Thompson <49499857+MrNoms@users.noreply.github.com>
2019-04-28 18:09:42 -0700

    Update ArrStack.cpp

--- ArrStack.cpp
+++ ArrStack.cpp
@@ -26,18 +26,48 @@ bool ArrStack<T>::full() {
 
 template <class T>
 T ArrStack<T>::pop() {
-	if (empty()) throw EmptyStack;
-	else {
-		top--;
-		return items[top + 1];
-	}
+	if (empty()) throw EmptyStack();
+	else return items[top--];
 }
 
 template <class T>
 void ArrStack<T>::add(T newItem) {
-	if (full()) throw FullStack;
-	else {
-		top++;
-		items[top] = newItem;
-	}
+	if (full()) throw FullStack();
+	else items[++top] = newItem;
+}#include "ArrStack.h"
+
+template <class T>
+ArrStack<T>::ArrStack(int cap) {
+	items = new T [cap];
+	capacity = cap;
+	top = -1;
+}
+
+template <class T>
+ArrStack<T>::~ArrStack() {
+	delete[] items;
+}
+
+template <class T>
+bool ArrStack<T>::empty() {
+	if (top == -1) return true;
+	else return false;
+}
+
+template <class T>
+bool ArrStack<T>::full() {
+	if (top == capacity - 1) return true;
+	else return false;
+}
+
+template <class T>
+T ArrStack<T>::pop() {
+	if (empty()) throw EmptyStack();
+	else return items[top--];
+}
+
+template <class T>
+void ArrStack<T>::add(T newItem) {
+	if (full()) throw FullStack();
+	else items[++top] = newItem;
 }
