b9ee108e7d5bc7a997870f39bbd210b9f7d9f99d
Rayshaun Thompson <49499857+MrNoms@users.noreply.github.com>
2019-04-30 13:10:38 -0700

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,10 +1,43 @@
 #pragma once
+#include <string>
 
-	enum Suit {NONE, DIAMOND, CLUB, HEART, SPADE};
-	enum Number {ZERO, TWO, THREE, FOUR, FIVE, SIX, SEVEN,
-		EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE};
+	enum Suit
+	{NONE, DIAMOND, CLUB, HEART, SPADE};
+	enum Number
+	{ZERO, TWO, THREE, FOUR, FIVE, SIX,
+		SEVEN, EIGHT, NINE, TEN, JACK,
+		QUEEN, KING, ACE};
 
 struct Card {
 	Suit thisSuit = NONE;
 	Number thisNum = ZERO;
+
+	std::string showSuit() {
+		switch (thisSuit) {
+		case DIAMOND: return "Diamond";
+			break;
+		case CLUB: return "Club";
+			break;
+		case HEART: return "Heart";
+			break;
+		case SPADE: return "Spade";
+			break;
+		}
+	}
+
+	char showNum() {
+		char num = '0';
+		if (thisNum && thisNum < JACK)
+			return thisNum + num;
+		switch (thisNum) {
+		case JACK: return 'J';
+			break;
+		case QUEEN: return 'Q';
+			break;
+		case KING: return 'K';
+			break;
+		case ACE: return 'A';
+			break;
+		}
+	}
 };
