6ae9172a1e0e671f8ce760ac6039cf482b192053
MrNoms <tiddle96@gmail.com>
2023-03-17 11:40:01 -0400

    Complete unit test for Card class

--- TexasHoldEm/src/main/include/Card.h
+++ TexasHoldEm/src/main/include/Card.h
@@ -1,52 +1,31 @@
 #pragma once
 #include <string>
+using namespace std;
+
+enum class Suit {DIAMOND, CLUB, HEART, SPADE};
+
+enum class Number {TWO, THREE, FOUR, FIVE, SIX,
+	SEVEN, EIGHT, NINE, TEN, JACK,
+	QUEEN, KING, ACE};
 
 struct Card {
-
-	enum class Suit {DIAMOND, CLUB, HEART, SPADE};
-
-	enum class Number {ACE, TWO, THREE, FOUR, FIVE, SIX,
-		SEVEN, EIGHT, NINE, TEN, JACK,
-		QUEEN, KING};
-
 	Suit cardSuit;
 	Number cardNum;
 
-	Card() {
-		*this = Card(Number::ACE, Suit::SPADE);
-	}
+	Card();
+	Card(Number n, Suit s);
 
-	Card(Number n, Suit s) {
-		cardSuit = s;
-		cardNum = n;
-	}
+	string showSuit();
 
-	std::string showSuit() {
-		switch (cardSuit) {
-		case Suit::DIAMOND: return "Diamond";
-		case Suit::CLUB: return "Club";
-		case Suit::HEART: return "Heart";
-		case Suit::SPADE: return "Spade";
-		default: return "NONE";
-		}
-	}
+	string showNum();
 
-	std::string showNum() {
-		std::string num;
-		if (cardNum > Number::ACE && cardNum < Number::JACK) {
-			num = (char)cardNum + '0';
-			return num;
-		}
-		else switch (cardNum) {
-		case Number::JACK: return "J";
-		case Number::QUEEN: return "Q";
-		case Number::KING: return "K";
-		case Number::ACE: return "A";
-		default: return "0";
-		}
-	}
+	int compare(Card);
 
-	std::string to_string() {
-		return showNum() + " " + showSuit();
-	}
+	string toString();
+
+	friend bool operator == (const Card&, const Card&);
+
+	friend bool operator > (const Card&, const Card&);
+
+	friend bool operator < (const Card&, const Card&);
 };
