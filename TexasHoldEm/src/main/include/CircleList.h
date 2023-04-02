9948d287a954966ddf4176031a0c0cc003771792
MrNoms <tiddle96@gmail.com>
2023-04-02 19:05:29 -0400

    Consolidate template class

--- TexasHoldEm/src/main/include/CircleList.h
+++ TexasHoldEm/src/main/include/CircleList.h
@@ -6,16 +6,75 @@ template <class T>
 class CircleList {
 private:
 	SingleLink<T> * end;
+	int size;
 
-	SingleLink<T>* findNode(T);
+	SingleLink<T>* findNodeBefore(T key) {
+		SingleLink<T>* i = end;
+		do {
+			if (i->next->data == key) return i;
+			else i = i->next;
+		} while (i != end);
+
+		return nullptr;
+	}
 	
 public:
-	CircleList();
-	~CircleList();
+	CircleList() {
+		end = nullptr;
+		size = 0;
+	}
+	~CircleList() {
+		SingleLink<T>* trash;
+		while (end != nullptr) {
+			trash = end;
+			end = end->next;
+			delete trash;
+		}
+	}
 
-	SingleLink<T> * begin();
-	void putNode(T);
-	void deleteNode(T);
-	int size();
-	std::string to_string();
+	SingleLink<T>* begin() {
+		return end->next;
+	}
+	bool putNode(T newData) {
+		SingleLink<T>* newNode;
+		try {
+			newNode = new SingleLink<T>(newData);
+		}
+		catch (std::runtime_error) {
+			return false;
+		}
+		if (end == nullptr) {
+			end = newNode;
+			end->next = newNode;
+		}
+		else {
+			newNode->next = end->next;
+			end->next = newNode;
+			end = newNode;
+		}
+		return true;
+	}
+	bool deleteNode(T trashData) {
+		SingleLink<T>* connect;
+		if ((connect = findNodeBefore(trashData)) != nullptr) {
+			SingleLink<T>* trash = connect->next;
+			connect->next = trash->next;
+			delete trash;
+			if (end == nullptr) end = connect;
+			return true;
+		}
+
+		return false;
+	}
+	int getSize() {
+		return size;
+	}
+	std::string toString() {
+		std::string str = "";
+		SingleLink<T>* j = end->next;
+		do {
+			str.append(j->toString());
+		} while (j != end->next);
+		return str.append("end");
+	}
 };
